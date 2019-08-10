56a6e944077985ce69d42458de70f891fa0dc8d7
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2019-08-10 05:16:35 +0100

    creating header, footer & form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
