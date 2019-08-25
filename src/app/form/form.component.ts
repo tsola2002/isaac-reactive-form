5d5f386fd333f320266e7b7403e11a553ec112c2
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2019-08-25 07:31:57 +0100

    using formbuilder to scaffold the reactive form with comments

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
-  selector: 'app-form',
+  selector: 'app-form', 
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
@@ -12,20 +12,30 @@ export class FormComponent implements OnInit {
   entryForm: FormGroup;
   
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
 
 
   
 
   ngOnInit(): void {
-          //formgroup is a collection of form controls where a form control belongs to a field  
-          this.entryForm = new FormGroup({
-            firstName: new FormControl(''),
-            lastName: new FormControl(''),
-            email: new FormControl(''),
-            sendCatalog: new FormControl(true)
-    });
+           //formbuilder takes in a control configuration object & defines he form controls associated with the form group
+           //you can have two options or specify an array
+          this.entryForm = this.fb.group({
+            firstName:'',
+            lastName:{value:'n/a', disabled:true},
+            email:'',
+            sendCatalog:true
+          });
+
+          //formgroup is a collection of form controls where a form control belongs to a field
+          //THE OLD WAY OF DOING FORM CONTROL
+          // this.entryForm = new FormGroup({
+          //   firstName: new FormControl(''),
+          //   lastName: new FormControl(''),
+          //   email: new FormControl(''),
+          //   sendCatalog: new FormControl(true)
+          //});
   } 
 
   //use setValue for all formcontrols & use Patch value for some formcontrols
